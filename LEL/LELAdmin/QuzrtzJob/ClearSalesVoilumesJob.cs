a35d7f3f8bee92c6f51d972c495da23d9579360b
xiechaoyuan <xiechaoyuan@ehomepoct.cn>
2019-06-15 11:39:40 +0800

    1

---
+++ LEL/LELAdmin/QuzrtzJob/ClearSalesVoilumesJob.cs
@@ -0,0 +1,21 @@
+﻿using Quartz;
+using Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace LELAdmin.QuzrtzJob
+{
+    public class ClearSalesVoilumesJob : IJob
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+            
+            GoodsService GoodsBLL = new GoodsService();
+            //System.Threading.Thread.Sleep(5000);
+            return GoodsBLL.ClearSalesVolumesAsync();
+        }
+    }
+}
---
+++ LEL/LELAdmin/QuzrtzJob/JobScheduler.cs
@@ -0,0 +1,38 @@
+﻿using Quartz;
+using Quartz.Impl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LELAdmin.QuzrtzJob
+{
+    /// <summary>
+    /// 调度程序
+    /// </summary>
+    public class JobScheduler
+    {
+        public async System.Threading.Tasks.Task startQuartzAsync()
+        {
+
+            //创建调度任务 
+            StdSchedulerFactory ssf = new StdSchedulerFactory();
+            IScheduler sched = await ssf.GetScheduler();
+            JobDetailImpl jdBossReport = new JobDetailImpl("jdBossReport", typeof(ClearSalesVoilumesJob));
+
+            ITrigger triggerBossReport = TriggerBuilder.Create()
+                .WithIdentity("trigger1", "group1")
+                 //.WithCronSchedule("*/10 * * * * ?")
+                 // .WithCronSchedule("0 0 1 * * *")//0 0 1 * * ?
+                 // .WithCronSchedule("0/5 * * * * ?") //每隔5秒执行
+                 //.WithCronSchedule("12 12 09 * * ?")//0 0 1 * * ?
+                 .WithCronSchedule("0 59 23 L * ?")
+                .Build(); ;
+
+            await sched.ScheduleJob(jdBossReport, triggerBossReport);
+
+            await sched.Start();
+
+        }
+    }
+}
