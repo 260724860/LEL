8b16c7215f61663bd5bd351cd8233f757649650f
xiechaoyuan <xiechaoyuan@ehomepoct.cn>
2019-06-20 10:50:37 +0800

    1

---
+++ LEL/DTO/Others/AddressDto.cs
@@ -0,0 +1,10 @@
+﻿namespace DTO.Others
+{
+    public class AddressDto
+    {
+        public string ReceiveName { get; set; }
+        public string ReceivePhone { get; set; }
+        public string ReceiveArea { get; set; }
+        public string ReceiveAddress { get; set; }
+    }
+}
