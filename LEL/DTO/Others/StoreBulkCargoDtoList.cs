d0589f56274c79db5b5737d1494642a958b6d88a
xiechaoyuan <xiechaoyuan@ehomepoct.cn>
2019-07-20 11:17:15 +0800

    1

---
+++ LEL/DTO/Others/StoreBulkCargoDtoList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO.Others
+{
+    public class StoreBulkCargoDtoList
+    {
+        public int ID { get; set; }
+        public int UserID { get; set; }
+        public string BarCode { get; set; }
+        public string StoreCode { get; set; }
+        public string Name { get; set; }
+        public int GoodsID { get; set; }
+        public string a { get; set; }
+        public string b { get; set; }
+        public string c { get; set; }
+        public string d { get; set; }
+    }
+}
